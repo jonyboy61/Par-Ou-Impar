ed82f4b7bd9a99dc901af37aecc53dd8ec56958b
jonyboy61 <jpasouto2013@gmail.com>
2022-06-06 23:54:53 +0100

    Adicionar arquivos de projeto.

---
+++ AppNumeroImpar/AppNumeroImpar/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppNumeroImpar.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppNumeroImpar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new PageInicial());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
